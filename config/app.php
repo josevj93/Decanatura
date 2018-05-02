c66ef0b9a4945fce90b6fc7021c5cefed2d1d11e
Giovanni <giovanni.jbs@hotmail.com>
2018-05-02 11:17:37 -0600

    Creacion de Controladora de Login

--- config/app.php
+++ config/app.php
@@ -222,16 +222,16 @@ return [
             'className' => 'Cake\Database\Connection',
             'driver' => 'Cake\Database\Driver\Mysql',
             'persistent' => false,
-            'host' => 'localhost',
+            'host' => 'decanatura.mysql.database.azure.com',
             /**
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'root',
-            'password' => '',
-            'database' => 'grupo01decanatura',
+            'username' => 'ecci@decanatura',
+            'password' => 'Gaby1234',
+            'database' => 'decanatura',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
             'flags' => [],
