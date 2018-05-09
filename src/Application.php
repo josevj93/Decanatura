37f5eb81ed97885227245d67c0e338beb28dc4b9
daniel134 <daquez96@hotmail.com>
2018-05-09 09:38:58 -0600

    -

--- src/Application.php
+++ src/Application.php
@@ -2,6 +2,7 @@
 /**
  * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
  * Licensed under The MIT License
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
